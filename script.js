93f3cf88bf18df64140bdfdb77d468f79be7df14
Frnak Kynard <fkynard2@gmail.com>
2022-02-22 01:58:20 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const searchBar = document.getElementById('searchBar');
+const url =
+  'https://yts.mx/api/v2/list_movies.json?sort_by=download_count&limit=50';
+const urlR = 'https://yts.mx/api/v2/movie_suggestions.json?movie_id=4';
+let ytsMovies = [];
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  randomMovies();
+});
+const randomMovies = async () => {
+  try {
+    const res = await fetch(urlR);
+    ytsMovies = await res.json();
+    displayMovies(ytsMovies.data.movies);
+    // console.log(ytsMovies);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+  const filteredMovies = ytsMovies.data.movies.filter((movie) => {
+    return movie.title_english.toLowerCase().includes(searchString);
+  });
+  displayMovies(filteredMovies);
+});
+
+const loadMovies = async () => {
+  try {
+    const res = await fetch(url);
+    ytsMovies = await res.json();
+    displayMovies(ytsMovies.data.movies);
+    // console.log(ytsMovies);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const displayMovies = (movie_list) => {
+  const htlmString = movie_list
+    .map((movie) => {
+      return `
+    <li class="movie">
+    <img src="${movie.medium_cover_image}"></img>
+    <h2>${movie.title_long}
+    <p>${movie.description_full}
+
+    </li>
+    `;
+    })
+    .join('');
+  movieList.innerHTML = htlmString;
+};
+
+loadMovies();
+// Create a way for the Movies to be type in search bar and displayed on the screen by title
+// Create a way for the Movie infromation and image to be displayed on the screen
+// Create a way for type of a movie to be loaded
+// Create a way for there to be a random search for 4 movies
